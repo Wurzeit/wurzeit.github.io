ed177c2534ae6fe0d19b54a7ca7852e0aa4caf70
wurzeit <hiccap.master.jedi@gmail.com>
2022-07-01 21:06:40 +0900

    add site contents

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,5 +1,29 @@
 import type { FC } from 'react'
 
 export const HomePagePresenter: FC = () => {
-  return <h1>Hi! It's Wurzeit</h1>
+  return (
+    <>
+      <p>Hi! I'm a student studying programming:)</p>
+      <p>Bio</p>
+      <p>2004 Born in Fukuoka(福岡), Japan.</p>
+      <p>2019 Entered in Notional Institute of Technology, Kurume Collage.</p>
+      <p>Love</p>
+      <p>Walk, Drawing, Coffee, Training</p>
+      <p>Interest</p>
+      <p>Operating systems, Compilers, Machine learning</p>
+      <p>Available programming languages</p>
+      <p>Rust, Scala, Typescript</p>
+      <p>On the web</p>
+      <p>
+        <a href="https://github.com/wurzeit">Github</a>
+      </p>
+      <p>
+        Hosted on Github Pages. Source:{' '}
+        <a href="https://github.com/wurzeit/wurzeit.github.io">
+          wurzeit/wurzeit.github.io
+        </a>
+      </p>
+      <p>&copy; Wurzeit 2022</p>
+    </>
+  )
 }
