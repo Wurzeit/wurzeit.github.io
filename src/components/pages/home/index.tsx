01ab2d55ef5a729f431475fd7aca9f0f915158a2
wurzeit <hiccap.master.jedi@gmail.com>
2022-07-01 16:35:06 +0900

    create homepage

---
+++ src/components/pages/home/index.tsx
@@ -0,0 +1,5 @@
+import type { FC } from 'react'
+
+export const HomePagePresenter: FC = () => {
+  return <h1>Hi! It's Wurzeit</h1>
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react'
+import { HomePagePresenter } from 'components/pages/home'
+
+const HomePage: FC = () => {
+  return <HomePagePresenter />
+}
+
+export default HomePage
